a1b8f2ae009982c4dd9888c2c3920517ec70845f
Guillermo Alvarez <gjalvarez09@gmail.com>
2018-10-20 00:32:18 -0600

    creacion del layout de la calculadora

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <calculadora></calculadora>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import calculadora from "./components/calculadora.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    calculadora
   }
 };
 </script>
---
+++ src/components/calculadora.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="main">
+        <h1>Calculadora sencilla</h1>
+
+        <div class="calc">
+            <input type="text">
+            <button id="c">C</button>
+            <button id="division">/</button>
+            <button id="mult">*</button>
+            <button id="menos">-</button>
+            <button id="siete">7</button>
+            <button id="ocho">8</button>
+            <button id="nueve">9</button>
+            <button id="mas">+</button>
+            <button id="cuatro">4</button>
+            <button id="cinco">5</button>
+            <button id="seis">6</button>
+            <button id="uno">1</button>
+            <button id="dos">2</button>
+            <button id="tres">3</button>
+            <button id="igual">=</button>
+            <button id="cero">0</button>
+            <button id="punto">.</button>
+        </div>
+    </div>
+</template>
+
+
+<script>
+    export default {
+
+    }
+</script>
+
+
+<style scoped>
+.main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.calc {
+    width: 300px;
+    height: 402px;
+    border: 1px solid black;
+    display: grid;
+    grid-template-columns: repeat(4, 75px);
+    grid-template-rows: repeat(6, 67px);
+}
+
+.calc input {
+    grid-column: 1 / span 4;
+}
+
+#mas {
+    grid-column-start: 4;
+    grid-row: 3 / span 2;
+}
+
+#igual {
+
+}
+
+
+</style>
