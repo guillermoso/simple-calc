073fa977a389d613d9bb90da182b83bd64a9b378
Guillermo Alvarez <gjalvarez09@gmail.com>
2018-10-22 21:21:58 -0600

    primeros estilos

--- src/components/calculadora.vue
+++ src/components/calculadora.vue
@@ -42,12 +42,28 @@
 .calc {
     width: 300px;
     height: 402px;
-    border: 1px solid black;
     display: grid;
     grid-template-columns: repeat(4, 75px);
     grid-template-rows: repeat(6, 67px);
 }
 
+.calc button, input {
+    margin: 2px;
+}
+
+.calc button {
+    border: 2px solid #141414;
+    background-color: #141414;
+    transition-duration: 0.4s;
+    border-radius: 2px;
+    font-size: 1.2em;
+    color: white;
+}
+
+.calc button:hover {
+    background-color: white;
+    color: #141414;
+}
 .calc input {
     grid-column: 1 / span 4;
 }
@@ -58,8 +74,12 @@
 }
 
 #igual {
-
+    grid-column-start: 4;
+    grid-row: 5 / span 2;
 }
 
+#cero {
+    grid-column: 1 / span 2;
+}
 
 </style>
