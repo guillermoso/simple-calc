c3e6f95757f855de7b8776efc8497972b1846cdd
Guillermo Alvarez <gjalvarez09@gmail.com>
2018-10-30 16:06:05 -0700

    funcionalidad de operadores

--- src/components/calculadora.vue
+++ src/components/calculadora.vue
@@ -5,20 +5,20 @@
         <div class="calc">
             <input type="text" v-model="num">
             <button id="c" @click="reset()">C</button>
-            <button id="division">/</button>
-            <button id="mult">*</button>
-            <button id="menos">-</button>
+            <button id="division" @click="dividir()">/</button>
+            <button id="mult" @click="multiplicar()">*</button>
+            <button id="menos" @click="restar()">-</button>
             <button id="siete" @click="capturar(7)">7</button>
             <button id="ocho" @click="capturar(8)">8</button>
             <button id="nueve" @click="capturar(9)">9</button>
-            <button id="mas">+</button>
+            <button id="mas" @click="sumar()">+</button>
             <button id="cuatro" @click="capturar(4)">4</button>
             <button id="cinco" @click="capturar(5)">5</button>
             <button id="seis" @click="capturar(6)">6</button>
             <button id="uno" @click="capturar(1)">1</button>
             <button id="dos" @click="capturar(2)">2</button>
             <button id="tres" @click="capturar(3)">3</button>
-            <button id="igual">=</button>
+            <button id="igual" @click="igual()">=</button>
             <button id="cero" @click="capturar(0)">0</button>
             <button id="punto">.</button>
         </div>
@@ -30,15 +30,57 @@
     export default {
           data: function () {
         return {
-            num: 0
+            num: 0,
+            op1: 0,
+            op2: 0,
+            operacion: 0
             }
         },
         methods: {
-            capturar: function (input) {
+            capturar(input) {
                this.num = (this.num * 10) + input;
             },
             reset() {
                 this.num = 0;
+            },
+            sumar(){
+                this.op1 = this.num;
+                this.num = 0;
+                this.operacion = 1;                
+            },
+            restar(){
+                this.op1 = this.num;
+                this.num = 0;
+                this.operacion = 2;      
+            },
+            multiplicar(){
+                this.op1 = this.num;
+                this.num = 0;
+                this.operacion = 3; 
+            },
+            dividir(){
+                this.op1 = this.num;
+                this.num = 0;
+                this.operacion = 4; 
+            },
+            igual(){
+                this.op2 = this.num;
+                switch (this.operacion) {
+                    case 1:
+                        this.num = this.op1 + this.op2;
+                        break;
+                    case 2:
+                        this.num = this.op1 - this.op2;
+                        break;
+                    case 3:
+                        this.num = this.op1 * this.op2;
+                        break;
+                    case 4:
+                        this.num = this.op1 / this.op2;
+                        break;
+                    default:
+                        break;
+                }
             }
         }
     }
@@ -51,6 +93,7 @@
     flex-direction: column;
     align-items: center;
 }
+
 .calc {
     width: 300px;
     height: 402px;
@@ -65,6 +108,7 @@
 
 .calc input {
     text-align: right;
+    font-size: 2em;
 }
 
 .calc button {
